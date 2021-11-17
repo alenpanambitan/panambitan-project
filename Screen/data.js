6e7e4280f61dfb085a29a91c94c8e8d4184be1ba
alenpanambitan <alen.panambitan@swu.phinma.edu.ph>
2021-11-17 21:04:09 +0800

    asdwa

---
+++ Screen/data.js
@@ -0,0 +1,35 @@
+const song = [
+    {
+        title: "Binhi",
+        artist: "Arthur Nery",
+        image: require('../assets/binhi.jpg'),
+        id: '1',
+    },
+    {
+        title: "coco",
+        artist: "Pewdiepie",
+        image: require('../assets/coco.jpg'),
+        id: '2',
+    },
+    {
+        title: "Romeo and Juliet",
+        artist: "Hobo Johnson",
+        image: require('../assets/hobo.jpg'),
+        id: '3',
+    },
+    {
+        title: "lunod",
+        artist: "Ben&Ben",
+        image: require('../assets/lunod.jpg'),
+        id: '4',
+    },
+    {
+        title: "Smells like teen spirit",
+        artist: "Nirvana",
+        image: require('../assets/nirvana.jpg'),
+        id: '5',
+    },
+];
+
+
+export default song;
