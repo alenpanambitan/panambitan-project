30d2c66bb502c202cebe812d0a500e42ed9d86c8
alenpanambitan <alen.panambitan@swu.phinma.edu.ph>
2021-11-17 20:03:57 +0800

    -

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Player from './component/Home';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const App = () => {
+  return(
+    <Player/>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
---
+++ component/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+export default function Player() {
+    return (
+        <SafeAreaView>
+            <Text>
+                Fuck!
+            </Text>
+        </SafeAreaView>
+    )
+}
