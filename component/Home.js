516baab752cf28349b51bc80439f64c6c0298cf6
alenpanambitan <alen.panambitan@swu.phinma.edu.ph>
2021-11-18 00:27:22 +0800

    Connected my functions for my buttons from home to controller

--- component/Home.js
+++ component/Home.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Dimensions, SafeAreaView, StyleSheet, Text, View, Image, Animated } from 'react-native';
+import { Dimensions, SafeAreaView, StyleSheet, Text, View, Image, Animated, ImageBackground } from 'react-native';
 import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
 import songs from './data';
+import Controller from './controller';
 // import Animated from 'react-native-reanimated';
 
 const {width, height} = Dimensions.get('window');
@@ -11,13 +12,35 @@ export default function Player() {
 
     const scrollX = useRef(new Animated.Value(0)).current;
     const [index, setIndex] = useState(0);
+    const slider = useRef(null)
+
+
+    
+
+
+
 
     useEffect (() => {
         scrollX.addListener(({value}) =>{
             const index = Math.round(value/width)
             setIndex(index);
         })
-    })
+    });
+
+
+    const Next = ()=>{
+        slider.current.scrollToOffset({
+            offset: (index + 1) * width,
+        })
+    }
+
+    const Prev = ()=>{
+        slider.current.scrollToOffset({
+            offset: (index - 1) * width,
+        })
+    }
+
+   
 
     const renderItem = ({item}) =>{
         return (
@@ -29,40 +52,47 @@ export default function Player() {
     }
 
     return (
-        <View style={styles.container}>
-            <SafeAreaView style={{height: 320}}>
-                <FlatList
-                    data = {songs}
-                    renderItem = {renderItem}
-                    keyExtractor = {(item) => item.id}
-                    horizontal
-                    showsHorizontalScrollIndicator = {false}
-                    pagingEnabled
-                    scrollEventThrottle = {16}
-                    onScroll = {Animated.event([{nativeEvent: {contentOffset: {x: scrollX}}}],
-                        {useNativeDriver: false},
-                        )}
-                />
-                
-                
-                
-                {/* <Image style={styles.img} source={require('../assets/coco.jpg')} />
-                <View style={styles.pbtn} >
-                    <TouchableOpacity style={styles.btn}>
-                        <Text style={styles.text}>prev</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.btn}>
-                        <Text style={styles.text}>play</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.btn}>
-                        <Text style={styles.text}>next</Text>
-                    </TouchableOpacity>
-                </View> */}
-                
-            </SafeAreaView>
-            <Text style={styles.title}>{songs[index].title}</Text>
-            <Text style={styles.artist}>{songs[index].artist}</Text>
-        </View>
+        <ImageBackground style={{width:'100%', height: '100%'}} source={require('../assets/bg.jpg')}>
+            <View style={styles.container}>
+            
+                <SafeAreaView style={{height: 320}}>
+                    <FlatList
+                        data = {songs}
+                        ref={slider}
+                        renderItem = {renderItem}
+                        keyExtractor = {(item) => item.id}
+                        horizontal
+                        showsHorizontalScrollIndicator = {false}
+                        pagingEnabled
+                        scrollEventThrottle = {16}
+                        onScroll = {Animated.event([{nativeEvent: {contentOffset: {x: scrollX}}}],
+                            {useNativeDriver: false},
+                            )}
+                    />
+                    
+                    
+                    
+                    {/* <Image style={styles.img} source={require('../assets/coco.jpg')} />
+                    <View style={styles.pbtn} >
+                        <TouchableOpacity style={styles.btn}>
+                            <Text style={styles.text}>prev</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.btn}>
+                            <Text style={styles.text}>play</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.btn}>
+                            <Text style={styles.text}>next</Text>
+                        </TouchableOpacity>
+                    </View> */}
+                    
+                </SafeAreaView>
+                <View>
+                    <Text style={styles.title}>{songs[index].title}</Text>
+                    <Text style={styles.artist}>{songs[index].artist}</Text>
+                </View>
+                <Controller onNext={Next} onPrv={Prev}/>
+            </View>
+        </ImageBackground>
         
         
         
@@ -100,12 +130,14 @@ const styles = StyleSheet.create({
         color: '#fff'
     },
     title: {
+        color: '#000',
         // bottom: 50,
         textAlign: 'center',
         fontSize: 26,
         textTransform:"capitalize",
     },
     artist: {
+        color: '#000',
         // bottom: 50,
         textAlign: 'center',
         fontSize: 20,
--- component/controller.js
+++ component/controller.js
@@ -2,19 +2,19 @@ import React from "react";
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import {MaterialIcons} from '@expo/vector-icons'
 
-export default function Controller() {
+export default function Controller({onNext,onPrv}) {
     return(
         <View style={styles.container}>
             <TouchableOpacity>
                 <MaterialIcons name='shuffle' size={30}/>
             </TouchableOpacity>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={onPrv}>
                 <MaterialIcons name='skip-previous' size={50}/>
             </TouchableOpacity>
             <TouchableOpacity>
-                <MaterialIcons name='pause' size={50}/>
+                <MaterialIcons name='pause' size={65}/>
             </TouchableOpacity>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={onNext}>
                 <MaterialIcons name='skip-next' size={50}/>
             </TouchableOpacity>
             <TouchableOpacity>
